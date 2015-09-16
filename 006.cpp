5714e2d6a146d7e02a556ff3fc9bebf8d294f11d
Michael Pfeifer <mikepf97@gmail.com>
2015-09-16 13:16:24 -0600

    Added for loop

---
+++ 006.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int sumOfSquares = 0;
+    int squareOfSum = 0;
+    for(int i = 0; i <= 100; i++) {
+        squareOfSum += i;
+        sumOfSquares += pow(i,2);
+    }
+    squareOfSum = pow(squareOfSum,2);
+}
