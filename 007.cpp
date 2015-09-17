dfa9c1298c4b2bfdc3e8b818f9af54962beecf9b
Michael Pfeifer <mikepf97@gmail.com>
2015-09-16 22:06:10 -0600

    Output works, but wrong number

--- 007.cpp
+++ 007.cpp
@@ -6,9 +6,7 @@ using namespace std;
 bool isPrime(long int number) {
     bool prime = true;
     for(long int j = 2; j < sqrt(number) + 1; j++) {
-        cout << "j: " << j << endl;
         if(number % j == 0) {
-            cout << "not prime";
             prime = false;
             break;
         }
@@ -17,20 +15,21 @@ bool isPrime(long int number) {
 }
 
 int main() {
-    cout << isPrime(49) << endl;
-/*
-    int primes [10001];
+    int desired = 10001;
+    int primes [desired];
     primes [0] = 2;
-    for(int i = 1; i <= 10001; i++) {
+    long int newNumber;
+    for(int i = 1; i <= desired; i++) {
         cout << "i: " << i << endl;
         bool found = false;
-        long int newNumber = primes[i-1] + 1;
+        newNumber = primes[i-1] + 1;
         while(!found) {
             if(isPrime(newNumber)) {
                 primes[i] = newNumber;
+                found = true;
             }
             newNumber++;
         }
     }
-*/
+    cout << desired << "th prime: " << primes[desired] << endl;
 }
