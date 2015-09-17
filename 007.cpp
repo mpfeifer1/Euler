e605f6de9337d3e13a96ff2b67c719d563619f33
Michael Pfeifer <mikepf97@gmail.com>
2015-09-16 21:56:40 -0600

    Fixed isPrime

--- 007.cpp
+++ 007.cpp
@@ -3,21 +3,28 @@
 
 using namespace std;
 
-boolean isPrime(int number) {
-    boolean prime = true;
-    for(int j = 0; j < sqrt(number) + 1; j++) {
-        if((double)number / j == (double)number % j) {
+bool isPrime(long int number) {
+    bool prime = true;
+    for(long int j = 2; j < sqrt(number) + 1; j++) {
+        cout << "j: " << j << endl;
+        if(number % j == 0) {
+            cout << "not prime";
             prime = false;
+            break;
         }
     }
     return prime;
+}
 
 int main() {
+    cout << isPrime(49) << endl;
+/*
     int primes [10001];
     primes [0] = 2;
     for(int i = 1; i <= 10001; i++) {
+        cout << "i: " << i << endl;
         bool found = false;
-        int newNumber = primes[i-1] + 1;
+        long int newNumber = primes[i-1] + 1;
         while(!found) {
             if(isPrime(newNumber)) {
                 primes[i] = newNumber;
@@ -25,4 +32,5 @@ int main() {
             newNumber++;
         }
     }
+*/
 }
