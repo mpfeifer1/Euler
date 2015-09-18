d9c2af7143589a00218609fadb52889a0798bfba
Michael Pfeifer <mikepf97@gmail.com>
2015-09-17 22:11:26 -0600

    9 works correctly

--- 009.cpp
+++ 009.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 
 using namespace std;
 
 int main() {
+    setprecision(12);
     int desired = 1000;
-    int answer = 0;
-    for(int i = 1; i < desired / 3; i++) {
-        for(int j = 1; j < desired / 3; j++) {
+    double answer = 0;
+    for(int i = 1; i < desired / 2; i++) {
+        for(int j = 1; j < desired / 2; j++) {
             double number = sqrt(pow(i,2) + pow(j,2));
             if(floor(number) == number) {
                 cout << "Pyth triplet: " << i << " " << j << " " << number << endl;
                 if(number + i + j == desired) {
-                    cout << "Answer is: " << number * i * j << endl;
+                    answer = number * i * j;
+                    cout << "Answer is: " << answer << fixed << endl;
                 }
             }
         }
