e5d26fc087c6d824e6d182c099026e0e9f201e7a
Michael Pfeifer <mikepf97@gmail.com>
2015-09-17 22:41:06 -0600

    10 works for test case

--- 010.cpp
+++ 010.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+int getNextPrime(int number) {
+    while(true) {
+        bool isPrime = true;
+        number++;
+        int root = (int)sqrt(number);
+        for(int i = 2; i <= root; i++) {
+            if(number % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime) {
+            return number;
+        }
+    }
+}
+
 int main() {
-    
+    int max = 10;
+    long int currentPrime = 2;
+    long int sum = 0;
+    while(currentPrime < max) {
+        cout << "Current Prime: " << currentPrime << endl;
+        sum += currentPrime;
+        currentPrime = getNextPrime(currentPrime);
+    }
+    cout << "Sum: " << sum << endl;
 }
