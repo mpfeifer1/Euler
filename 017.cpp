87cb6281e7b8ccd0b6169cebe677d45a93d0923b
Michael Pfeifer <mikepf97@gmail.com>
2015-09-21 11:41:20 -0600

    Experimented with methods

--- 017.cpp
+++ 017.cpp
@@ -9,6 +9,45 @@ int main() {
     cout << "Letters in " << number << ": " << countLetters(number) << endl;
 }
 
+
+// Maxes out at 1000;
 int countLetters(int number) {
-    
+    int letters = 0;
+
+    int numberCopy = number;
+    // Count tens/hundreds/thousand
+    if(number == 1000) {
+        letters += 8; // 8 letters in "thousand"
+    }
+
+    // For thousands and hundreds
+    switch(digit) {
+        case: 1
+            count += 3;
+            break
+        case: 2
+            count += 3;
+            break;
+        case: 3
+            count += 5;
+            break;
+        case: 4
+            count += 4;
+            break
+        case: 5
+            count += 4;
+            break;
+        case: 6
+            count += 3;
+            break;
+        case: 7
+            count += 5;
+            break;
+        case: 8
+            count += 5;
+            break;
+        case: 9
+            count += 4;
+            break;
+    }
 }
