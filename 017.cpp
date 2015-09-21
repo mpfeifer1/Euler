29334bc97b99a8031c024dc3102e3c7df6fa5747
Michael Pfeifer <mikepf97@gmail.com>
2015-09-21 11:29:16 -0600

    Started project 17

---
+++ 017.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int countLetters(int number);
+
+int main() {
+    int number = 5;
+    cout << "Letters in " << number << ": " << countLetters(number) << endl;
+}
+
+int countLetters(int number) {
+    
+}
