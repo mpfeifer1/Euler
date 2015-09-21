28c05a4bc3b0ca1ca7dbfe629f65cd5214e55f13
Michael Pfeifer <mikepf97@gmail.com>
2015-09-21 13:25:54 -0600

    Added methods, and wrote program for last two digits

--- 017.cpp
+++ 017.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 int countLetters(int number);
+int digitToLetters(int digit);
+int TenstoLetters(int number);
 
 int main() {
     int number = 5;
@@ -19,35 +21,77 @@ int countLetters(int number) {
     if(number == 1000) {
         letters += 8; // 8 letters in "thousand"
     }
+}
 
-    // For thousands and hundreds
+int digitToLetters(int digit) {
     switch(digit) {
-        case: 1
-            count += 3;
-            break
-        case: 2
-            count += 3;
-            break;
-        case: 3
-            count += 5;
-            break;
-        case: 4
-            count += 4;
-            break
-        case: 5
-            count += 4;
-            break;
-        case: 6
-            count += 3;
-            break;
-        case: 7
-            count += 5;
-            break;
-        case: 8
-            count += 5;
-            break;
-        case: 9
-            count += 4;
-            break;
+        case 0:
+            return 0;
+        case 1:
+            return 3;
+        case 2:
+            return 3;
+        case 3:
+            return 5;
+        case 4:
+            return 4;
+        case 5:
+            return 4;
+        case 6:
+            return 3;
+        case 7:
+            return 5;
+        case 8:
+            return 5;
+        case 9:
+            return 4;
     }
+}
+
+int tensToLetters(int number) {
+
+    count = 0;
+
+    if(number >= 90) {
+        count += 6;
+    } else if(number >= 80) {
+        count += 6;
+    } else if(number >= 70) {
+        count += 7;
+    } else if(number >= 60) {
+        count += 5;
+    } else if(number >= 50) {
+        count += 5;
+    } else if(number >= 40) {
+        count += 6;
+    } else if(number >= 30) {
+        count += 6;
+    } else if(number >= 20) {
+        count += 6;
+    } else if(number == 19) {
+        count += 8;
+    } else if(number == 18) {
+        count += 8;
+    } else if(number == 17) {
+        count += 9;
+    } else if(number == 16) {
+        count += 7;
+    } else if(number == 15) {
+        count += 7;
+    } else if(number == 14) {
+        count += 8;
+    } else if(number == 13) {
+        count += 8;
+    } else if(number == 12) {
+        count += 6;
+    } else if(number == 11) {
+        count += 6;
+    } else if(number == 10) {
+        count += 3;
+    } else {
+        count += digitToLetters(number);
+    }
+
+    return count;
+
 }
