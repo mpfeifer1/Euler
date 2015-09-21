d020b8fac8344622c6aceba1bfc0df087f9e6d28
Michael Pfeifer <mikepf97@gmail.com>
2015-09-20 22:11:37 -0600

    Finds Length of longest chain

--- 014.cpp
+++ 014.cpp
@@ -3,5 +3,26 @@
 using namespace std;
 
 int main() {
-    
+    int maxChain = 0;
+
+    for(int i = 1; i < 1000000; i++) {
+        int chain = 0;
+        int n = i;
+
+        while(n > 1) {
+            if(n % 2 == 0) {
+                n = n/2;
+            }
+            else {
+                n = (3 * n) + 1;
+            }
+            chain++;
+        }
+
+        if(chain > maxChain) {
+            maxChain = chain;
+        }
+    }
+
+    cout << "Longest chain is " << maxChain << endl;
 }
