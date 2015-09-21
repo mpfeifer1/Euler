a37133e2bbdfc2b37b8dd77059d2cba55b900c11
Michael Pfeifer <mikepf97@gmail.com>
2015-09-21 11:07:50 -0600

    Converted to type double

--- 016.cpp
+++ 016.cpp
@@ -4,12 +4,13 @@
 using namespace std;
 
 int main() {
-    int power = 15; // Change to 1000
+    int power = 1000;
     long int sum = 0;
-    long int number = pow(2, power);
+    long unsigned double number = pow(2, power);
+    cout << "Number is " << number << endl;
 
     while(number > 0) {
-        int digit = number % 10;
+        int digit = number fmod(10);
         number /= 10;
         sum += digit;
     }
