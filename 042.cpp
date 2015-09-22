2dfad0a43fe9a8cc534296dd3b57de1a683c737c
Michael Pfeifer <mikepf97@gmail.com>
2015-09-22 09:10:08 -0600

    Fixed compiler error, added uppercase cases

--- 042.cpp
+++ 042.cpp
@@ -15,14 +15,15 @@ int main() {
 
     int index = 0; // Current spot in the string
 
-    // cout << word << endl; // Prints entire text file
+    isTriangle("Sky");
 }
 
 bool isTriangle(string word) {
     cout << "Processing word " << word << endl;
     int wordValue = 0;
     for(int i = 0; i < word.length(); i++) {
-        wordValue += charToInt((char)word.substr(i,1));
+        char temp = word[i];
+        wordValue += charToInt(temp);
     }
 
     int n = 1;
@@ -40,56 +41,82 @@ bool isTriangle(string word) {
 
 int charToInt(char c) {
     switch(c) {
+        case 'A':
         case 'a':
             return 1;
+        case 'B':
         case 'b':
             return 2;
+        case 'C':
         case 'c':
             return 3;
+        case 'D':
         case 'd':
             return 4;
+        case 'E':
         case 'e':
             return 5;
+        case 'F':
         case 'f':
             return 6;
+        case 'G':
         case 'g':
             return 7;
+        case 'H':
         case 'h':
             return 8;
+        case 'I':
         case 'i':
             return 9;
+        case 'J':
         case 'j':
             return 10;
+        case 'K':
         case 'k':
             return 11;
+        case 'L':
         case 'l':
             return 12;
+        case 'M':
         case 'm':
             return 13;
+        case 'N':
         case 'n':
             return 14;
+        case 'O':
         case 'o':
             return 15;
+        case 'P':
         case 'p':
             return 16;
+        case 'Q':
         case 'q':
             return 17;
+        case 'R':
         case 'r':
             return 18;
+        case 'S':
         case 's':
             return 19;
+        case 'T':
         case 't':
             return 20;
+        case 'U':
         case 'u':
             return 21;
+        case 'V':
         case 'v':
             return 22;
+        case 'W':
         case 'w':
             return 23;
+        case 'X':
         case 'x':
             return 24;
+        case 'Y':
         case 'y':
             return 25;
+        case 'Z':
         case 'z':
             return 26;
         default:
