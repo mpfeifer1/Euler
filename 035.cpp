b398cebbd1ed5c1a445ef87c6cd923a4102eeea2
Michael Pfeifer <mikepf97@gmail.com>
2015-09-26 14:31:11 -0600

    Cleaned up code

--- 035.cpp
+++ 035.cpp
@@ -10,14 +10,12 @@ int main() {
     int max = 1000000;
     int sum = 1; // Starts at 1 to include 2, which is skipped by this method
     for(int i = 3; i < max; i += 2) { // Skips every other because no even number is prime
-        //cout << "I is " << i << endl;
-        bool prime = true;
+        bool prime = true; // Defaults to true
         int j;
         int count;
-        for(j = i, count = i; count > 0; count /= 10) {
+        for(j = i, count = i; count > 0; count /= 10) { // Checks all rotations are prime
             j = rotate(j);
-            prime = prime && isPrime(j);
-            //cout << "J is " << prime << " prime" << endl;
+            prime = prime && isPrime(j); // makes prime false if rotation is false
         }
         if(prime) {
             cout << j << " is prime" << endl;
@@ -38,7 +36,7 @@ int rotate(int number) { // Moves the last digit to the first spot
     return number;
 }
 
-bool isPrime(int number) {
+bool isPrime(int number) { // Checks if a number is prime
     bool prime = true;
     for(int i = 2; i < sqrt(number) + 1; i++) {
         if(number % i == 0) {
