e4ca1c15ebab4fa26109efd796779ce894a3f6c1
Michael Pfeifer <mikepf97@gmail.com>
2015-09-28 13:33:20 -0600

    Added framework for 54

--- 054.cpp
+++ 054.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
-int main() {
+bool betterHand(); // Returns true if first is better, false if second is better
 
+int hasRoyalFlush();    // All methods return -1 if they don't have it
+int hasStraightFlush(); // Otherwise, they return the highest card in it
+int hasFourOfAKind();
+int hasFullHouse();
+int hasFlush();
+int hasStraight();
+int hasThreeOfAKind();
+int hasTwoPairs();
+int hasOnePair();
+int highestCard();
+
+int main() {
+    int oneWins = 0;
+    int numberOfHands = 5;
+    ifstream file("sources/054.txt");
+    string input;
+    string oneHand[5];
+    string twoHand[5];
+
+    for(int i = 0; i < numberOfHands; i++) { // Calculates every hand
+        getline(file, input);
+        for(int j = 0; j < 5; j++) { // Put the first 5 cards into the hand
+            
+        }
+        for(int j = 0; j < 5; j++) { // Put the last 5 cards into the other hand
+            
+        }
+
+        
+    }
 }
+
