c2d56e042f8b57b426c14499e8b8c3afabd529ac
Michael Pfeifer <mikepf97@gmail.com>
2015-10-01 16:38:21 -0600

    Optimized 1

--- 001.cpp
+++ 001.cpp
@@ -4,11 +4,10 @@ using namespace std;
 
 int main() {
 	int sum = 0;
-	for(int i = 0; i < 1000; i++) {
-		if((i%3 == 0) || (i%5 == 0)) {
+	for(int i = 0; i < 1000; i+= 3) {
+		if(i%5 == 0) {
 			sum += i;
-			cout << "I   = " << i << endl;
-			cout << "Sum = " << sum << endl;
 		}
 	}
+    cout << "Sum is " << sum << endl;
 }
