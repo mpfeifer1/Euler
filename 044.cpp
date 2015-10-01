daccb9eb320c05f036db1f0418b3500a8b90e8ce
Michael Pfeifer <mikepf97@gmail.com>
2015-10-01 16:03:34 -0600

    Started 44

---
+++ 044.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int pentagon(int number);
+
+int main() {
+    bool found = false;
+    while(!found) {
+
+    }
+}
+
+int pentagon(int number) {
+    return (number * (3 * number - 1)) / 2;
+}
+
+bool isPentagon(int number) {
+    return 
+}
