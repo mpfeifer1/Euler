ad72cd36b145a074853308ad9e2ab9d9d54e1f09
Michael Pfeifer <mikepf97@gmail.com>
2015-10-01 16:43:49 -0600

    Fixed 1

--- 001.cpp
+++ 001.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main() {
 	int sum = 0;
-	for(int i = 0; i < 1000; i+= 3) {
-		if(i%5 == 0) {
+	for(int i = 0; i < 1000; i++) {
+		if(i % 5 == 0 || i % 3 == 0) {
 			sum += i;
 		}
 	}
