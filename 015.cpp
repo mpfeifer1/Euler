92e54977a68964beedc6c3440cf8d14ac3aa7875
Michael Pfeifer <mikepf97@gmail.com>
2015-10-01 10:13:36 -0600

    Refactored to be more intuitive

--- 015.cpp
+++ 015.cpp
@@ -2,23 +2,24 @@
 
 using namespace std;
 
-long int factorial(int number);
+long int factorial(int movesNeeded, int sideLength);
 
 int main() {
-    int height = 20;
-    int length = 20;
+    int sideLength = 2;
 
-    int movesNeeded = height+length;
-    long int permutations = factorial(movesNeeded) /
-                       (factorial(length) * factorial(height));
+    int movesNeeded = 2 * sideLength;
+    long int permutations = factorial(movesNeeded, sideLength);
 
     cout << permutations << " different paths" << endl;
 }
 
-long int factorial(int number) {
+long int factorial(int movesNeeded, int sideLength) {
     long int sum = 1;
-    for(int i = number; i > 1; i--) {
+
+    for(int i = sideLength + 1; i <= movesNeeded; i++) {
         sum *= i;
     }
+
+    sum /= sideLength;
     return sum;
 }
