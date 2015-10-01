10640c4b9ff753b25e3669460ffee6569c41b740
Michael Pfeifer <mikepf97@gmail.com>
2015-10-01 05:33:53 -0600

    Added initial path

--- 015.cpp
+++ 015.cpp
@@ -3,9 +3,17 @@
 using namespace std;
 
 int main() {
-    int gridHeight = 2;
-    int gridLength = 2;
+    int height = 2;
+    int length = 2;
+    string path = "";
 
-    for(int 
+    for(int i = 0; i < length; i++) {
+        path += "R";
+    }
 
+    for(int i = 0; i < height; i++) {
+        path += "D";
+    }
+
+    // Calculate all unique permutations, like (RRDD), (RDRD)
 }
