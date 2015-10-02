c35732b889755db7c930c91dd77e2aef32e2002c
Michael Pfeifer <mikepf97@gmail.com>
2015-10-02 13:02:26 -0600

    Output values of arrays before loop

--- 025.cpp
+++ 025.cpp
@@ -10,6 +10,17 @@ int main() {
     array1[0] = 1;
     array2[0] = 1;
 
+    for(int i = desiredLength -1; i > 0; i--) {
+        cout << array1[i];
+    }
+    cout << endl << endl;
+
+     for(int i = desiredLength -1; i > 0; i--) {
+        cout << array2[i];
+    }
+    cout << endl << endl;
+
+
     while(array1[desiredLength - 1] < 0 && array2[desiredLength - 1] < 0) {
         // Adds array 1 to array 2
         int carry1 = 0;
