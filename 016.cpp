e63be8f25c5f7abdb4ce210715e779afab8d7027
Michael Pfeifer <mikepf97@gmail.com>
2015-10-02 11:07:22 -0600

    Refactored 16 to use an array

--- 016.cpp
+++ 016.cpp
@@ -1,19 +1,57 @@
+#include <algorithm>
 #include <iostream>
 #include <cmath>
 
 using namespace std;
 
 int main() {
-    int power = 1000;
-    long int sum = 0;
-    long unsigned double number = pow(2, power);
-    cout << "Number is " << number << endl;
+    int power = 4;
+    int sum = 0;
 
-    while(number > 0) {
-        int digit = number fmod(10);
-        number /= 10;
-        sum += digit;
+    int digits = 3; // A guess of the number of digits in 2^1000
+    int number[digits];
+    number[0] = 1;
+    for(int i = 1; i < digits - 1; i++) {
+        number[i] = 0;
     }
+
+    for(int i = 0; i < digits - 1; i++) {
+        cout << number[i] << endl;
+    }
+    cout << "Properly initialized" << endl;
+
+    bool carryNext = false;
+    bool carryThis = false;
+
+    for(int i = 0; i < power; i++) {
+        //cout << "i is " << i << endl;
+        for(int j = 0; j < digits - 1; j++) {
+            //cout << "j is " << j << endl;
+            int temp = number[i];
+            if((temp * 2) % 10 == temp * 2) {
+                number[i] = temp * 2;
+            } else {
+                number[i] = (temp * 2) % 10;
+                carryNext = true;
+            }
+
+            if(carryThis) {
+                number[i] += 1;
+                carryThis = false;
+            }
+
+            if(carryNext) {
+                carryNext = false;
+                carryThis = true;
+            }
+        }
+    }
+    cout << "Successfully carried" << endl;
+
+    for(int i = 0; i < digits; i++) {
+        cout << number[i];
+    }
+    cout << endl;
 
     cout << "Sum of the digits is " << sum << endl;
 }
