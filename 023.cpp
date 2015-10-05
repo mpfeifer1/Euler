75ed6a59e7ddf70d4ae88df166964402471a7ce6
Michael Pfeifer <mikepf97@gmail.com>
2015-10-05 13:13:51 -0600

    Added getSumOfFactors and isAbundant

--- 023.cpp
+++ 023.cpp
@@ -4,12 +4,30 @@ using namespace std;
 
 bool isAbundant(int number);
 bool isSumOfTwoAbundant(int number);
+int getSumOfFactors(int number);
 
 int main() {
     int sum = 0;
+    /*
     for(int i = 0; i <= 28123; i++)
         if(!isSumOfTwoAbundant(i)) {
             sum += i;
         }
     }
+    */
+    cout << "Sum is " << sum << endl;
+}
+
+int getSumOfFactors(int number) {
+    int sum = 0;
+    for(int i = 1; i <= number / 2; i++) {
+        if(number % i == 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+bool isAbundant(int number) {
+    return number < getSumOfFactors(number);
 }
