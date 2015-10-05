2eeebcfb7b57289a78c5c74b2b2f576d0dc693f5
Michael Pfeifer <mikepf97@gmail.com>
2015-10-04 21:16:27 -0600

    Solved 44

--- 044.cpp
+++ 044.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
+#include <iomanip>
+#include <cmath>
 
 using namespace std;
 
 int pentagon(int number);
+bool isPentagon(int number);
 
 int main() {
-    int search = 100; // How many pentagonal numbers to search
+    cout << fixed;
+    int search = 10000; // How many pentagonal numbers to search
     for(int i = 1; i < search; i++) {
         for(int j = 1; j < search; j++) {
-            // Check
-            if() { // If it's found, break out
-                
+            if( isPentagon(abs(pentagon(j)-pentagon(i))) && isPentagon(pentagon(i)+pentagon(j)) ) { // If it's found, break out
+                cout << "Difference is " << abs(pentagon(j) - pentagon(i)) << endl;
                 break;
             }
         }
@@ -22,6 +25,6 @@ int pentagon(int number) {
 }
 
 bool isPentagon(int number) {
-    double root = sqrt(1 + 24 * n);
-    return (root == int(root) && root % 6 == 5);
+    double root = sqrt(1 + 24 * number);
+    return (root == int(root) && (int)root % 6 == 5);
 }
