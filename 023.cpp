82844d710f87b1255258b30ff7353a444fb6bca0
Michael Pfeifer <mikepf97@gmail.com>
2015-10-06 10:27:23 -0600

    Commented everything, refactored to use an array canBeAbundant

--- 023.cpp
+++ 023.cpp
@@ -9,46 +9,43 @@ int main() {
     int sum = 0;
     int min = 12; // Smallest Abundant Number
     int max = 28123; // Largest Abundant Number necessary
-    int abundants[6965]; // Calculated total number of Abundants under 28123
+    int abundants[max]; // Calculated total number of Abundants under 28123
     int abundantsSize = 0;
     bool sumOfAbundants = false;
+    bool canBeAbundant[max + 1];
 
-    // Tested, Working
+    // Fills List with all abundant numbers, defaults canBeAbundant to false
     for(int i = min; i <= max; i++) {
         if(isAbundant(i)) {
             abundants[abundantsSize] = i;
             abundantsSize++;
-            cout << i << endl;
         }
+        canBeAbundant[i] = false;
     }
 
-    cout << "Abundants list complete" << endl;
 
-    for(int num = 1; num <= max; num++) {
-        //cout << "Num is " << num << endl;
-        sumOfAbundants = true;
-
-        for(int i = 0; i <= abundantsSize && sumOfAbundants; i++) {
-            for(int j = i; j <= abundantsSize && sumOfAbundants; j++) {
-
-                if(abundants[i] + abundants[j] == num) {
-                    sumOfAbundants = true;
-                }
-                else {
-                    sumOfAbundants = false;
-                }
-
+    // Fills canBeAbundant list with true if a number can be written as a sum of abundants
+    for(int i = 0; i <= abundantsSize; i++) {
+        for(int j = i; j <= abundantsSize; j++) {
+            if(abundants[i] + abundants[j] <= max) {
+                canBeAbundant[abundants[i] + abundants[j]] = true;
+            } else {
+                break;
             }
         }
+    }
 
-        if(!sumOfAbundants) {
-            sum += num;
+    // Sums all numbers that can't be abundant
+    for(int i = 0; i <= max; i++) {
+        if(!canBeAbundant[i]) {
+            sum += i;
         }
     }
+
     cout << "Sum is " << sum << endl;
 }
 
-// Tested, Working
+// Gets the sum of factors
 int getSumOfFactors(int number) {
     int sum = 0;
     for(int i = 1; i <= number / 2; i++) {
@@ -59,7 +56,7 @@ int getSumOfFactors(int number) {
     return sum;
 }
 
-// Tested, Working
+// Determinese whether number is abundant
 bool isAbundant(int number) {
     return number < getSumOfFactors(number);
 }
