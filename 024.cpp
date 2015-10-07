f07a06ba76193c882063fb38a7ae8405eb6de836
Michael Pfeifer <mikepf97@gmail.com>
2015-10-06 18:59:16 -0600

    Added swap method

--- 024.cpp
+++ 024.cpp
@@ -2,16 +2,14 @@
 
 using namespace std;
 
+void swapIndex(int i, int j, int number[]);
+
 int main() {
     int number[] = {0, 1, 2}; // Leave it in numerical order
     int size = 3; // Number of values in above array
     int permutations = 1; // Times to permute number
 
-    for(int i = 0; i < permutations; i++) {
-        int temp = number[i];
-        number[i] = number[i+1];
-        number[i+1] = temp;
-    }
+    swapIndex(2,1, number);
 
     // Print out number
     cout << "Number is ";
@@ -19,4 +17,10 @@ int main() {
         cout << number[i];
     }
     cout << endl;
+}
+
+void swapIndex(int i, int j, int number[]) {
+    int temp = number[i];
+    number[i] = number[j];
+    number[j] = temp;
 }
