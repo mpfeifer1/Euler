20d28d8afb6be45fd0e81906fb2ea2005cb67ffa
Michael Pfeifer <mikepf97@gmail.com>
2015-10-06 19:49:43 -0600

    Started switching to combinatorics

--- 024.cpp
+++ 024.cpp
@@ -2,14 +2,26 @@
 
 using namespace std;
 
-void swapIndex(int i, int j, int number[]);
+void removeIndex(int i, int length, int number[]);
+int factorial(int number);
 
 int main() {
-    int number[] = {0, 1, 2}; // Leave it in numerical order
-    int size = 3; // Number of values in above array
-    int permutations = 1; // Times to permute number
+    int initial[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}; // Leave it in numerical order
+    int length = 10;
+    string permuted = "";
+    int remain = 1000000 - 1; // Times to permute number
 
-    swapIndex(2,1, number);
+    // Combinatorics
+    // The last 9 digits can be ordered in 9! ways, so the first 362880 permutations start with 0
+    for(int i = 1; i < 10; i++) {
+        // Calculates how many permutations for the first number
+        int j = remain / factorial(length - i);
+        int remain %= factorial(length - i);
+
+        // Places the number of permutations found in the next "Available" slot in the number
+        permuted += initial[j];
+        initial.
+    }
 
     // Print out number
     cout << "Number is ";
@@ -19,8 +31,18 @@ int main() {
     cout << endl;
 }
 
-void swapIndex(int i, int j, int number[]) {
-    int temp = number[i];
-    number[i] = number[j];
-    number[j] = temp;
+// Swaps 2 array values
+void removeIndex(int i, int length, int number[]) {
+    for(int j = i; j < length; j++) {
+        
+    }
+}
+
+// Calculates a number's factorial
+int factorial(int number) {
+    int product = 0;
+    for(int i = number; number > 0; number--) {
+        product *= number;
+    }
+    return product;
 }
