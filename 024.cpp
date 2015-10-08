4f5c01bcb6be0c7a9ece62fa488a6ceb0d5e9e97
Michael Pfeifer <mikepf97@gmail.com>
2015-10-08 07:58:02 -0600

    Messed with off-by-one errors, added couts

--- 024.cpp
+++ 024.cpp
@@ -9,7 +9,7 @@ int factorial(int number);
 
 int main() {
     static const int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}; // Initialize array in numeric order
-    vector<int> initial (arr, arr + sizeof(arr) / sizeof(arr[0])); // Copy array to vector
+    vector<int> initial(arr, arr + sizeof(arr) / sizeof(arr[0])); // Copy array to vector
 
     int n = initial.size();
     string permuted = "";
@@ -19,20 +19,24 @@ int main() {
     // The last 9 digits can be ordered in 9! ways, so the first 362880 permutations start with 0
     for(int i = 1; i < n; i++) {
         // Calculates how many permutations for the first number
-        int j = remain / factorial(initial[n - i]);
-        remain %= factorial(n - i);
+        int j = remain / (int)factorial(initial[n - i]);
+        remain %= factorial(initial[n - i]);
 
+        cout << "Remain is " << remain << endl;
         cout << "J is " << j << endl;
+        cout << "initial[j] is " << initial[j] << endl;
 
         // Places the number of permutations found in the next "Available" slot in the number
-        permuted += to_string(initial[j]);
+        permuted += initial[j];
         initial.erase(initial.begin() + j);
 
         // Print out number
         cout << "Number is ";
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < initial.size(); i++) {
             cout << initial[i];
         }
+        cout << endl;
+
         cout << endl;
 
         // Break out early
