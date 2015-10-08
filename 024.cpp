fe4cf6ab4bab1c6f4c0797292b82307c3e94c08e
Michael Pfeifer <mikepf97@gmail.com>
2015-10-08 09:53:47 -0600

    Removed unnecessary outputs

--- 024.cpp
+++ 024.cpp
@@ -15,6 +15,8 @@ int main() {
     string permuted = "";
     int remain = 1000000 - 1; // Times to permute number
 
+    cout << "Number is ";
+
     // Combinatorics
     // The last 9 digits can be ordered in 9! ways, so the first 362880 permutations start with 0
     for(int i = 1; i < n; i++) {
@@ -22,31 +24,18 @@ int main() {
         int j = remain / (int)factorial(arr[n - i]);
         remain %= factorial(arr[n - i]);
 
-        cout << "Remain is " << remain << endl;
-        cout << "J is " << j << endl;
-        cout << "initial[j] is " << initial[j] << endl;
+        cout << initial[j];
 
         // Places the number of permutations found in the next "Available" slot in the number
         permuted += initial[j];
         initial.erase(initial.begin() + j);
-
-        // Print out number
-        cout << "Number is ";
-        for(int i = 0; i < initial.size(); i++) {
-            cout << initial[i];
-        }
-        cout << endl;
-
-        cout << endl;
 
         // Break out early
         if(remain == 0) {
             break;
         }
     }
-
-    // Print out number
-    cout << "Number is " << permuted << endl;
+    cout << endl;
 }
 
 // Calculates a number's factorial
