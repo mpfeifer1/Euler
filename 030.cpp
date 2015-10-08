067c4ee8cad0e033ac07cf7fbd901a20e79e74af
Michael Pfeifer <mikepf97@gmail.com>
2015-10-08 10:44:02 -0600

    Made code more readable

--- 030.cpp
+++ 030.cpp
@@ -15,8 +15,14 @@ int main() {
         for(b = 0; b < 10; b++) {
             for(c = 0; c < 10; c++) {
                 for(d = 0; d < 10; d++) {
-                    int number = (a * 1000) + (b * 100) + (c * 10) + (d * 1);
-                    int powers = pow(a, 4) + pow(b, 4) + pow(c, 4) + pow(d, 4);
+                    int number = (a * 1000) +
+                                 (b * 100) +
+                                 (c * 10) +
+                                 (d * 1);
+                    int powers = pow(a, 4) +
+                                 pow(b, 4) +
+                                 pow(c, 4) +
+                                 pow(d, 4);
                     if(number == powers) {
                         cout << "Solution: " << number << endl;
                         sum += number;
