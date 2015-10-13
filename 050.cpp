44a53c8b935ecbdc1f83359383308c36fa7c83c2
Michael Pfeifer <mikepf97@gmail.com>
2015-10-13 15:51:31 -0600

    Started 50

---
+++ 050.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace main;
+
+bool isPrime(int number);
+
+int main() {
+    // Count all
+}
+
+isPrime(int number) {
+    // Create an array, fill with all primes under 1million, then use it to check
+    static int primes[];
+}
