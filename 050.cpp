e5331880451863f28bd8c5df8162c47b06bb11a0
Michael Pfeifer <mikepf97@gmail.com>
2015-10-13 18:10:30 -0600

    Started prime functions

--- 050.cpp
+++ 050.cpp
@@ -2,13 +2,28 @@
 
 using namespace main;
 
+int getPrime(int index);
 bool isPrime(int number);
 
 int main() {
     // Count all
 }
 
-isPrime(int number) {
+int getPrime(int index) {
     // Create an array, fill with all primes under 1million, then use it to check
-    static int primes[];
+    static int primes[79000]; // Guess of the number of primes under 1 million
+
+    primes[0] = 0;
+    primes[1] = 1;
+
+    
+}
+
+bool isPrime(int number) {
+    for(int i = 2; i < sqrt(number) + 1; i++) {
+        if(number % i == 0) {
+            return false
+        }
+    }
+    return true;
 }
