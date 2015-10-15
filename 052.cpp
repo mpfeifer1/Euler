195a74212814a23b57e5cf6b778ed4ff336c7117
Michael Pfeifer <mikepf97@gmail.com>
2015-10-15 10:45:25 -0600

    Fixed converting number to string

--- 052.cpp
+++ 052.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -8,7 +9,7 @@ bool permutatedMultiple(long int number);
 int main() {
     long int number = 1;
     bool found = false;
-    while(!found) {
+    while(!found && number < 1000000) {
         if(permutatedMultiple(number)) {
             found = true;
             cout << "Permutated Multiple: " << number << endl;
@@ -18,10 +19,13 @@ int main() {
 }
 
 bool permutated(long int number1, long int number2) {
-    
+    string num1 = to_string(number1);
+    string num2 = to_string(number2);
+    cout << "num1: " << num1 << endl;
+    return false;
 }
 
-bool permuatedMultiple(long int number) {
+bool permutatedMultiple(long int number) {
     return (permutated(number, 2 * number) &&
             permutated(number, 3 * number) &&
             permutated(number, 4 * number) &&
