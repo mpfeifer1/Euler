27d5b6bd1a00f3e87c52e89ed7c369c972a1439e
Michael Pfeifer <mikepf97@gmail.com>
2015-10-18 21:14:47 -0600

    Added basic structure

--- 059.cpp
+++ 059.cpp
@@ -1,15 +1,34 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
+int xor(char data, char cipher);
 int toAscii(char c);
 
 int main() {
+    // Import from file
     ifstream file("sources/059.txt");
     string message;
     getline(file, message);
+
+    // Parse file to array
+    vector<int> characters();
+    int index = 0;
+    while(still stuff in message) {
+        get stuff until ,
+        move forward index
+    }
+
+    // Find encryption key
+
+    // Decrypt file
+
+    // (Temporary) Output message
+
+    // Sum ASCII characters
 }
 
 int xor(char data, char cipher) {
