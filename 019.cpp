4fd9a1a253d10b03499e45f6c98380f50ca205bc
Michael Pfeifer <mikepf97@gmail.com>
2015-10-19 08:50:36 -0600

    Started 19

---
+++ 019.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int sundays = 0;
+    for(every year) {
+        // Catch weird cases
+        for(every month) {
+            // Catch weird cases
+            for(every week) {
+                // Catch weird cases
+                sundays++;
+            }
+        }
+    }
+}
