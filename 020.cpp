b6b6add39fb29437105d632b6485e3ac5d0a6fbb
Michael Pfeifer <mikepf97@gmail.com>
2015-10-29 05:07:31 -0600

    Works for values under 10

---
+++ 020.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+void multiply(int factorial[], int number);
+
+#define SIZE 160
+
+int main() {
+    int factorial[SIZE] = {1}; // Threre are 158 digits in 100!
+
+    for(int i = 1; i < 10; i++) {
+        multiply(factorial, i);
+    }
+
+    for(int i = 0; i < SIZE; i++) {
+        cout << factorial[i];
+    }
+    cout << endl;
+
+    return 0;
+}
+
+void multiply(int factorial[], int number) {
+    int carry = 0;
+    for(int i = 0; i < SIZE; i++) {
+        factorial[i] = (factorial[i] * number) + carry;
+        carry = factorial[i] / 10;
+        factorial[i] %= 10;
+    }
+}
