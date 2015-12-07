cb6be7b6dc652ed048490394753aeec3ce9d9e10
Michael Pfeifer <mikepf97@gmail.com>
2015-12-06 23:14:46 -0700

    Made 7 more efficient, made it follow standards

--- 007.cpp
+++ 007.cpp
@@ -4,23 +4,20 @@
 using namespace std;
 
 bool isPrime(long int number) {
-    bool prime = true;
     for(long int j = 2; j < sqrt(number) + 1; j++) {
         if(number % j == 0) {
-            prime = false;
-            break;
+            return false;
         }
     }
-    return prime;
+    return true;
 }
 
 int main() {
     int desired = 10001;
-    int primes [desired];
-    primes [0] = 2;
+    int primes[desired];
+    primes[0] = 2;
     long int newNumber;
     for(int i = 1; i <= desired; i++) {
-        cout << "i: " << i << endl;
         bool found = false;
         newNumber = primes[i-1] + 1;
         while(!found) {
