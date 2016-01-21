00e8d4d84d4ec13f44b332033560d16e9fdfa869
Michael Pfeifer <mikepf97@gmail.com>
2016-01-21 21:09:04 +0000

    Removed unnecessary variable

--- 009.cpp
+++ 009.cpp
@@ -7,14 +7,12 @@ using namespace std;
 int main() {
     setprecision(12);
     int desired = 1000;
-    double answer = 0;
     for(int i = 1; i < desired / 2; i++) {
         for(int j = 1; j < desired / 2; j++) {
             double number = sqrt(pow(i,2) + pow(j,2));
             if(floor(number) == number) {
                 if(number + i + j == desired) {
-                    answer = number * i * j;
-                    cout << "Answer is: " << answer << fixed << endl;
+                    cout << "Answer is: " << number * i * j << fixed << endl;
                 }
             }
         }
