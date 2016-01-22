01a21cc8dcbd09fb75161154a19ec9f50674d811
Michael Pfeifer <mikepf97@gmail.com>
2016-01-22 11:19:32 -0700

    Shortened code further

--- 009.cpp
+++ 009.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
-#include <iomanip>
 #include <cmath>
-
 using namespace std;
 
 int main() {
-    setprecision(12);
     int desired = 1000;
     for(int i = 1; i < desired / 2; i++) {
         for(int j = 1; j < desired / 2; j++) {
-            double number = sqrt(pow(i,2) + pow(j,2));
+            double number = sqrt(i * i + j * j);
             if(floor(number) == number) {
                 if(number + i + j == desired) {
-                    cout << "Answer is: " << number * i * j << fixed << endl;
+                    cout << "Answer is: " << number * i * j << endl;
                 }
             }
         }
