4591a13cceb311dad3c6002fbe96f8ba3380f5c9
Michael Pfeifer <mikepf97@gmail.com>
2018-03-09 23:24:35 -0700

    Solved Poker Hands

--- 054.cpp
+++ 054.cpp
@@ -1,292 +1,349 @@
-#include <algorithm>
-#include <iostream>
-#include <sstream>
-#include <fstream>
-#include <string>
+// Michael Pfeifer
+// Version 04, 2018/02/17
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 
 using namespace std;
+using namespace __gnu_pbds;
 
-bool betterHand(string handOne, string handTwo); // Returns true if first is better, false if second is better
-char toCardNotation(int num);
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int uint;
+typedef unsigned long long ull;
 
-int hasRoyalFlush    (string hand); // All methods return -1 if they don't have it
-int hasStraightFlush (string hand); // Otherwise, they return the highest card in it
-int hasFourOfAKind   (string hand);
-int hasFullHouse     (string hand);
-int hasFlush         (string hand);
-int hasStraight      (string hand);
-int hasThreeOfAKind  (string hand);
-int hasTwoPairs      (string hand);
-int hasPair          (string hand);
-int hasPair          (string hand, int skip);
+const ld  pi   = 4.0*atanl(1.0);
+const int iinf = 1e9 + 10;
+const ll  inf  = 1e18 + 10;
+const int mod  = 1000000007;
+const ld  prec = .000001;
 
-int highestCard      (string hand);
-int highestValue     (string hand);
+#define enld endl
+#define endl '\n'
+#define pb push_back
+#define debug(x) cout<<#x<<" -> "<<x<<'\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define uni(x) (x).erase(unique(all(x)), (x).end())
+#define rep(i, n) for (ll i = 0; i < (ll)(n); ++i)
+#define rep1(i, n) for (ll i = 1; i <= (ll)(n); ++i)
+#define umap unordered_map
+#define uset unordered_set
+
+template<class TIn>
+using indexed_set = tree<
+        TIn, null_type, less<TIn>,
+        rb_tree_tag, tree_order_statistics_node_update>;
+
+void fast() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+}
+
+int HANDS = 1000;
+void file() {
+    auto a = freopen("sources/054.txt",  "r", stdin);
+    //auto a = freopen("temp.txt",  "r", stdin);
+    if(!a) cout << "uh oh" << endl;
+}
+
+
+
+
+
+struct card {
+    int val;
+    char suit;
+};
+
+card getcard(string s) {
+    card c;
+    c.suit = s[1];
+    c.val = s[0] - '0';
+    c.val++;
+
+    if(s[0] == 'A') {
+        c.val = 15;
+    }
+    if(s[0] == 'K') {
+        c.val = 14;
+    }
+    if(s[0] == 'Q') {
+        c.val = 13;
+    }
+    if(s[0] == 'J') {
+        c.val = 12;
+    }
+    if(s[0] == 'T') {
+        c.val = 11;
+    }
+    return c;
+}
+
+bool cmp1(card& c1, card& c2) {
+    if(c1.val == c2.val) {
+        return c1.suit < c2.suit;
+    }
+    return c1.val < c2.val;
+}
+
+int royalflush(vector<card> hand) {
+    bool works = true;
+    sort(all(hand), cmp1);
+    for(int i = 0; i <= 3; i++) {
+        if(hand[i].suit != hand[i+1].suit) {
+            works = false;
+        }
+        if(hand[i].val+1 != hand[i+1].val) {
+            works = false;
+        }
+    }
+    if(hand[4].val != 15) {
+        works = false;
+    }
+
+    if(works) {
+        return 1;
+    }
+    return 0;
+}
+
+int straightflush(vector<card> hand) {
+    bool works = true;
+    sort(all(hand), cmp1);
+    for(int i = 0; i <= 3; i++) {
+        if(hand[i].suit != hand[i+1].suit) {
+            works = false;
+        }
+        if(hand[i].val+1 != hand[i+1].val) {
+            works = false;
+        }
+    }
+
+    if(works) {
+        return 1;
+    }
+    return 0;
+}
+
+int fourofakind(vector<card> hand) {
+    map<int,int> c;
+    for(auto i : hand) {
+        c[i.val]++;
+    }
+    for(auto i : c) {
+        if(i.second == 4) {
+            return i.first;
+        }
+    }
+    return 0;
+}
+
+int fullhouse(vector<card> hand) {
+    map<int,int> c;
+    for(auto i : hand) {
+        c[i.val]++;
+    }
+    int two = 0;
+    int three = 0;
+    for(auto i : c) {
+        if(i.second == 2) {
+            two = i.first;
+        }
+        if(i.second == 3) {
+            three = i.first;
+        }
+    }
+    if(two && three) {
+        return three*100+two;
+    }
+    return 0;
+}
+
+int flush(vector<card> hand) {
+    set<char> seen;
+    for(auto i : hand) {
+        seen.insert(i.suit);
+    }
+
+    if(seen.size() == 1) {
+        return 1;
+    }
+    return 0;
+}
+
+int straight(vector<card> hand) {
+    bool works = true;
+    sort(all(hand), cmp1);
+    for(int i = 0; i <= 3; i++) {
+        if(hand[i].val+1 != hand[i+1].val) {
+            works = false;
+        }
+    }
+
+    if(works) {
+        return 1;
+    }
+    return 0;
+}
+
+int threeofakind(vector<card> hand) {
+    map<int,int> c;
+    for(auto i : hand) {
+        c[i.val]++;
+    }
+    for(auto i : c) {
+        if(i.second == 3) {
+            return i.first;
+        }
+    }
+    return 0;
+}
+
+int twopairs(vector<card> hand) {
+    map<int,int> c;
+    for(auto i : hand) {
+        c[i.val]++;
+    }
+
+    int pair1 = 0;
+    int pair2 = 0;
+
+    for(auto i : c) {
+        if(i.second == 2) {
+            if(pair1 == 0) {
+                pair1 = i.first;
+            }
+            else {
+                pair2 = i.first;
+            }
+        }
+    }
+
+    if(pair2 == 0) {
+        return 0;
+    }
+
+    if(pair1 > pair2) {
+        swap(pair1,pair2);
+    }
+
+    return pair2*100+pair1;
+}
+
+int onepair(vector<card> hand) {
+    map<int,int> c;
+    for(auto i : hand) {
+        c[i.val]++;
+    }
+
+    int best = 0;
+    for(auto i : c) {
+        if(i.second == 2) {
+            best = max(best, i.first);
+        }
+    }
+
+    return best;
+}
+
+int highcard(vector<card> hand1, vector<card> hand2) {
+    // Returns true if hand1 wins
+    sort(all(hand1), cmp1);
+    sort(all(hand2), cmp1);
+    reverse(all(hand1));
+    reverse(all(hand2));
+
+    for(int i = 0; i < 5; i++) {
+        if(hand1[i].val > hand2[i].val) {
+            return 1;
+        }
+        if(hand1[i].val < hand2[i].val) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
 
 int main() {
-    int oneWins = 0;
-    int numberOfHands = 10;
+    file();
+    //fast();
 
-    ifstream file("sources/054.txt");
-    string input;
+    int wins1 = 0;
+    int wins2 = 0;
 
-    string handTwo;
-    string handOne;
+    for(int i = 0; i < HANDS; i++) {
+        vector<card> hand1(5);
+        vector<card> hand2(5);
 
-    for(int i = 0; i < numberOfHands; i++) { // Calculates every hand
-
-        getline(file, input);
-        input.erase(remove(input.begin(), input.end(), ' '), input.end());
-
-        handOne = input.substr(0, 10);
-        handTwo = input.substr(10,10);
-
-        cout << handOne << " " << handTwo << endl;
-        //cout << hasPair(handOne) << endl;
-
-        if(betterHand(handOne, handTwo)) {
-            oneWins++;
-            cout << "PLayer One Wins" << endl;
-        } else {
-            cout << "Player Two Wins" << endl;
+        for(auto&i : hand1) {
+            string s;
+            cin >> s;
+            i = getcard(s);
+        }
+        for(auto&i : hand2) {
+            string s;
+            cin >> s;
+            i = getcard(s);
         }
 
-        cout << endl;
-    }
+        vector<int> val1(10,false);
+        vector<int> val2(10,false);
 
-    cout << "Player 1 won " << oneWins << " times" << endl;
-}
+        val1[0] = royalflush(hand1);
+        val1[1] = straightflush(hand1);
+        val1[2] = fourofakind(hand1);
+        val1[3] = fullhouse(hand1);
+        val1[4] = flush(hand1);
+        val1[5] = straight(hand1);
+        val1[6] = threeofakind(hand1);
+        val1[7] = twopairs(hand1);
+        val1[8] = onepair(hand1);
 
-bool betterHand(string handOne, string handTwo) {
-    if(hasRoyalFlush(handOne) > 0 || hasRoyalFlush(handTwo) > 0) {
-        cout << "Someone has a royal flush" << endl;
-        return hasRoyalFlush(handOne) > hasRoyalFlush(handTwo);
-    }
-    if(hasStraightFlush(handOne) > 0 || hasStraightFlush(handTwo) > 0) {
-        cout << "Someone has a straight flush" << endl;
-        return hasStraightFlush(handOne) > hasStraightFlush(handTwo);
-    }
-    if(hasFourOfAKind(handOne) > 0 || hasFourOfAKind(handTwo) > 0) {
-        cout << "Someone has four of a kind" << endl;
-        return hasFourOfAKind(handOne) > hasFourOfAKind(handTwo);
-    }
-    if(hasFullHouse(handOne) > 0 || hasFullHouse(handTwo) > 0) {
-        cout << "Someone has a full house" << endl;
-        return hasFullHouse(handOne) > hasFullHouse(handTwo);
-    }
-    if(hasFlush(handOne) > 0 || hasFlush(handTwo) > 0) {
-        cout << "Someone has a flush" << endl;
-        if(hasFlush(handOne) == hasFlush(handTwo)) {
-            // Find 2nd, 3rd, 4th, 5th
-        }
-        return hasFlush(handOne) > hasFlush(handTwo);
-    }
-    if(hasStraight(handOne) > 0 || hasStraight(handTwo) > 0) {
-        cout << "Someone has a straight" << endl;
-        return hasStraight(handOne) > hasStraight(handTwo);
-    }
-    if(hasThreeOfAKind(handOne) > 0 || hasThreeOfAKind(handTwo) > 0) {
-        cout << "Someone has three of a kind" << endl;
-        return hasThreeOfAKind(handOne) > hasThreeOfAKind(handTwo);
-    }
-    if(hasTwoPairs(handOne) > 0 || hasTwoPairs(handTwo) > 0) {
-        cout << "Someone has two pairs" << endl;
-        return hasTwoPairs(handOne) > hasTwoPairs(handTwo);
-    } // Detect highest pair, second highest pair, then leftover high card
-    if(hasPair(handOne) > 0 ||  hasPair(handTwo) > 0) {
-        cout << "Someone has a pair" << endl;
-        return hasPair(handOne) > hasPair(handTwo);
-    } // Detect higher pair, then leftover of highest cards
-    cout << "Only high cards" << endl;
-    return highestCard(handOne) > highestCard(handTwo);
-      // if those are tied, detect 2nd, 3rd, 4th, and 5th
-}
+        val2[0] = royalflush(hand2);
+        val2[1] = straightflush(hand2);
+        val2[2] = fourofakind(hand2);
+        val2[3] = fullhouse(hand2);
+        val2[4] = flush(hand2);
+        val2[5] = straight(hand2);
+        val2[6] = threeofakind(hand2);
+        val2[7] = twopairs(hand2);
+        val2[8] = onepair(hand2);
 
-int hasRoyalFlush(string hand) {
-    if(hasFlush(hand) > 0 &&
-       hand.find("A") != string::npos &&
-       hand.find("K") != string::npos &&
-       hand.find("Q") != string::npos &&
-       hand.find("J") != string::npos &&
-       hand.find("T") != string::npos) {
-        return 1;
-    }
-    return -1;
-}
+        bool added = false;
+        for(int i = 0; i < 9; i++) {
+            if(val1[i] == val2[i] && val1[i] > 0) {
+                break;
+            }
+            if(val1[i] == val2[i] && val1[i] == 0) {
+                continue;
+            }
 
-int hasStraightFlush(string hand) {
-    if(hasFlush(hand) > 0 && hasStraight(hand) > 0) {
-        return highestCard(hand);
-    }
-    return -1;
-}
+            added = true;
+            if(val1[i] > val2[i]) {
+                wins1++;
+            }
+            if(val1[i] < val2[i]) {
+                wins2++;
+            }
 
-int hasFlush(string hand) {
-    if(count(hand.begin(), hand.end(), 'H') == 5 ||
-       count(hand.begin(), hand.end(), 'C') == 5 ||
-       count(hand.begin(), hand.end(), 'D') == 5 ||
-       count(hand.begin(), hand.end(), 'S') == 5) {
-        return highestCard(hand);
-    }
-    return -1;
-}
-
-int hasFullHouse(string hand) {
-    if(hasPair(hand) > 0 && hasThreeOfAKind(hand) > 0) {
-        return hasThreeOfAKind(hand);
-    }
-    return -1;
-}
-
-int hasFourOfAKind(string hand) {
-    for(int a = 1; a <= 13; a++) {
-        if(count(hand.begin(), hand.end(), toCardNotation(a)) == 4) {
-            return a;
-        }
-    }
-    return -1;
-}
-
-int hasThreeOfAKind(string hand) {
-    for(int a = 1; a <= 13; a++) {
-        if(count(hand.begin(), hand.end(), toCardNotation(a)) == 3) {
-            return a;
-        }
-    }
-    return -1;
-}
-
-int hasPair(string hand) {
-    return hasPair(hand, -1);
-}
-
-int hasPair(string hand, int skip) {
-    for(int a = 1; a <= 13; a++) {
-        if(a == skip) {
-            a++;
-        }
-        if(count(hand.begin(), hand.end(), toCardNotation(a)) == 2) {
-            return a;
-        }
-    }
-    return -1;
-}
-
-int hasTwoPairs(string hand) {
-    int valueOne = hasPair(hand);
-    int valueTwo = hasPair(hand, valueOne);
-
-    if(valueOne > 0 && valueTwo > 0) {
-
-        if(valueOne > valueTwo) {
-            return valueOne;
-        } else if(valueOne < valueTwo) {
-            return valueTwo;
-        } else {
-            // TODO check last card
-            return valueOne;
+            break;
         }
 
-    }
-    return -1;
-}
-
-int hasStraight(string hand) {
-    int highest = highestValue(hand);
-    bool hasStraight = true;
-    for(int i = 1; i <= 5; i++) {
-        if(!(hand.find(toCardNotation(highest - i)) != string::npos)) {
-            hasStraight = false;
+        if(!added) {
+            if(highcard(hand1,hand2)) {
+                wins1++;
+            }
+            else {
+                wins2++;
+            }
         }
+
+
     }
-    if(hasStraight) {
-        return highest;
-    }
-    return -1;
-}
+    cout << wins1 << " " << wins2 << endl;
 
-int highestCard(string hand) {
-
-    if(hand.find("A") != string::npos) {
-        return 1;
-    } else if(hand.find("K") != string::npos) {
-        return 13;
-    } else if(hand.find("Q") != string::npos) {
-        return 12;
-    } else if(hand.find("J") != string::npos) {
-        return 11;
-    } else if(hand.find("T") != string::npos) {
-        return 10;
-    } else if(hand.find("9") != string::npos) {
-        return 9;
-    } else if(hand.find("8") != string::npos) {
-        return 8;
-    } else if(hand.find("7") != string::npos) {
-        return 7;
-    } else if(hand.find("6") != string::npos) {
-        return 6;
-    } else if(hand.find("5") != string::npos) {
-        return 5;
-    } else if(hand.find("4") != string::npos) {
-        return 4;
-    } else if(hand.find("3") != string::npos) {
-        return 3;
-    } else if(hand.find("2") != string::npos) {
-        return 2;
-    } else {
-        cout << "Highest number can't find a valid nunber" << endl;
-    }
-
-    return -1;
-}
-
-int highestValue(string hand) {
-
-    if(hand.find("K") != string::npos) {
-        return 13;
-    } else if(hand.find("Q") != string::npos) {
-        return 12;
-    } else if(hand.find("J") != string::npos) {
-        return 11;
-    } else if(hand.find("T") != string::npos) {
-        return 10;
-    } else if(hand.find("9") != string::npos) {
-        return 9;
-    } else if(hand.find("8") != string::npos) {
-        return 8;
-    } else if(hand.find("7") != string::npos) {
-        return 7;
-    } else if(hand.find("6") != string::npos) {
-        return 6;
-    } else if(hand.find("5") != string::npos) {
-        return 5;
-    } else if(hand.find("4") != string::npos) {
-        return 4;
-    } else if(hand.find("3") != string::npos) {
-        return 3;
-    } else if(hand.find("2") != string::npos) {
-        return 2;
-    } else if(hand.find("A") != string::npos) {
-        return 1;
-    } else {
-        cout << "Highest number can't find a valid nunber" << endl;
-    }
-
-    return -1;
-}
-
-char toCardNotation(int num) {
-    switch(num) {
-        case 13:
-            return 'K';
-        case 12:
-            return 'Q';
-        case 11:
-            return 'J';
-        case 10:
-            return 'T';
-        case 1:
-            return 'A';
-    }
-    return char(num);
+    return 0;
 }
