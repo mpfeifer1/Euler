77c2dff5a7215954c661fb1a0f9b949ddc9f01f2
Michael Pfeifer <mikepf97@gmail.com>
2018-03-11 21:10:43 -0600

    Solved Large non-Mersenne Prime

--- 097.py
+++ 097.py
@@ -1,1 +1,13 @@
-print(28433*2**7830457+1)
+def fastpow(base, exp, mod):
+    base %= mod
+    res = 1
+    while exp > 0:
+        if exp % 2 == 1:
+            res = (res * base) % mod
+        base = (base * base) % mod
+        exp //= 2
+    return res % mod
+
+
+big = 10000000000
+print((28433 * fastpow(2, 7830457, big) + 1) % big)
